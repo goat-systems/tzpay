ed30d8534a7f3bc7256311854ab132bf56262254
Brice Aldrich <40796124+DefinitelyNotAGoat@users.noreply.github.com>
2020-03-07 10:37:38 -0500

    carthage and edesk storage (#13)

---
+++ cli/internal/db/db.go
@@ -0,0 +1,47 @@
+package db
+
+import (
+	"fmt"
+	"os/user"
+
+	"github.com/boltdb/bolt"
+	"github.com/pkg/errors"
+)
+
+type bucketKey string
+
+const (
+	walletBucket       bucketKey = "WALLETBUCKET"
+	walletBucketSecret bucketKey = "edesk"
+)
+
+// DB wraps bolt db functions
+type DB struct {
+	bolt *bolt.DB
+}
+
+// Open will open or create the tzpay boltdb profile
+func Open(path string) (*DB, error) {
+	if path == "" {
+		usr, err := user.Current()
+		if err != nil {
+			return &DB{}, errors.Wrap(err, "failed to open boltdb")
+		}
+		path = fmt.Sprintf("%s.tzpay/tzpay.db", usr)
+	}
+	db, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		return &DB{}, errors.Wrap(err, "failed to open boltdb")
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists([]byte(walletBucket))
+		if err != nil {
+			return fmt.Errorf("create bucket: %s", err)
+		}
+		return nil
+	})
+
+	return &DB{
+		bolt: db,
+	}, nil
+}
---
+++ cli/internal/db/db_test.go
@@ -0,0 +1,16 @@
+package db
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Open(t *testing.T) {
+	db, err := Open("./tzpay.db")
+	assert.Nil(t, err)
+	assert.NotNil(t, db)
+	err = os.Remove("./tzpay.db")
+	assert.Nil(t, err)
+}
